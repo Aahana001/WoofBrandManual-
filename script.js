0097d0045794b6ba6106776b71af7c013c02a966
Aahana001 <132641382+Aahana001@users.noreply.github.com>
2024-04-13 20:11:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let startX;
 let currentX;
 let threshold = 50; // Adjust this threshold value as needed
 
-const images = ["wof1.jpg", "wof2.jpg","wof3.jpg", "wof4.jpg"]; // Add your image URLs here
+const images = ["1.JPG", "2.JPG","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG","8.JPG","9.JPG","10.JPG","11.JPG","12.JPG","13.JPG","14.JPG","15.JPG","16.JPG","17.JPG","18.JPG","19.JPG","20.JPG","21.JPG","22.JPG","23.JPG","24.JPG","25.JPG","26.JPG","27.JPG","28.JPG","29.JPG","30.JPG","31.JPG","32.JPG","33.JPG","34.JPG","35.JPG","36.JPG","37.JPG","38.JPG","39.JPG","40.JPG","41.JPG","42.JPG","43.JPG","44.JPG","45.JPG","46.JPG","47.JPG","48.JPG","49.JPG","50.JPG","51.JPG","52.JPG","53.JPG","54.JPG","55.JPG"]; // Add your image URLs here
 let currentIndex = 0;
 
 updateCard();
