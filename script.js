dff0e79c6239246923925d607efde96bc490a1f3
Aahana001 <aahanahm@gmail.com>
2024-04-15 13:16:02 +0530

    'final'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let startX;
 let currentX;
 let threshold = 50; // Adjust this threshold value as needed
 
-const images = ["1.JPG", "2.JPG","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG","8.JPG","9.JPG","10.JPG","11.JPG","12.JPG","13.JPG","14.JPG","15.JPG","16.JPG","17.JPG","18.JPG","19.JPG","20.JPG","21.JPG","22.JPG","23.JPG","24.JPG","25.JPG","26.JPG","27.JPG","28.JPG","29.JPG","30.JPG","31.JPG","32.JPG","33.JPG","34.JPG","35.JPG","36.JPG","37.JPG","38.JPG","39.JPG","40.JPG","41.JPG","42.JPG","43.JPG","44.JPG","45.JPG","46.JPG","47.JPG","48.JPG","49.JPG","50.JPG","51.JPG","52.JPG","53.JPG","54.JPG","55.JPG"]; // Add your image URLs here
+const images = ["1.jpg", "2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg","29.jpg","30.jpg","31.jpg","32.jpg","33.jpg","34.jpg","35.jpg","36.jpg","37.jpg","38.jpg","39.jpg","40.jpg","41.jpg","42.jpg","43.jpg","44.jpg","45.jpg","46.jpg","47.jpg","48.jpg","49.jpg","50.jpg","51.jpg","52.jpg","53.jpg","54.jpg","55.jpg"]; // Add your image URLs here
 let currentIndex = 0;
 
 updateCard();
